955bf19c751bc9ff9784fe3ec8f602501110c891
eLmincfrLu <elmincfrlu@gmail.com>
2026-02-01 11:26:58 +0000

    python

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,17 +8,17 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left_height, right_height;
+	size_t left_height, right_height;
 
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-    if (left_height > right_height)
-        return (left_height + 1);
-    return (right_height + 1);
+	if (left_height > right_height)
+		return (left_height + 1);
+	return (right_height + 1);
 }
 
 /**
@@ -30,13 +30,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    size_t left_height, right_height;
+	size_t left_height, right_height;
 
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    left_height = (tree->left) ? binary_tree_height(tree->left) : 0;
-    right_height = (tree->right) ? binary_tree_height(tree->right) : 0;
+	left_height = (tree->left) ? binary_tree_height(tree->left) : 0;
+	right_height = (tree->right) ? binary_tree_height(tree->right) : 0;
 
-    return ((int)left_height - (int)right_height);
+	return ((int)left_height - (int)right_height);
 }
