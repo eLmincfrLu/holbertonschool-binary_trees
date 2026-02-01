063f62347ab687554e2121edbb98a2c8a7ab4d97
eLmincfrLu <elmincfrlu@gmail.com>
2026-02-01 11:25:10 +0000

    python

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,42 +1,42 @@
-#include "binary_trees.h"
+3#include "binary_trees.h"
 
 /**
  * binary_tree_height - Measures the height of a binary tree
- * @tree: Pointer to the root node of the tree
+ * @tree: Pointer to the root node
+ *
  * Return: Height of the tree, 0 if tree is NULL
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+    size_t left_height, right_height;
 
-	if (tree == NULL)
-		return (0);
+    if (tree == NULL)
+        return (0);
 
-	if (tree->left == NULL && tree->right == NULL)
-		return (0);
+    left_height = binary_tree_height(tree->left);
+    right_height = binary_tree_height(tree->right);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	if (left_height > right_height)
-		return (left_height + 1);
-	return (right_height + 1);
+    if (left_height > right_height)
+        return (left_height + 1);
+    return (right_height + 1);
 }
 
 /**
  * binary_tree_balance - Measures the balance factor of a binary tree
- * @tree: Pointer to the root node of the tree
- * Return: Balance factor (left_height - right_height), 0 if tree is NULL
+ * @tree: Pointer to the root node
+ *
+ * Return: Balance factor (left_height - right_height)
+ *         0 if tree is NULL
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left_height, right_height;
+    size_t left_height, right_height;
 
-	if (tree == NULL)
-		return (0);
+    if (tree == NULL)
+        return (0);
 
-	left_height = (int)binary_tree_height(tree->left);
-	right_height = (int)binary_tree_height(tree->right);
+    left_height = (tree->left) ? binary_tree_height(tree->left) : 0;
+    right_height = (tree->right) ? binary_tree_height(tree->right) : 0;
 
-	return (left_height - right_height);
+    return ((int)left_height - (int)right_height);
 }
