d3f461b1d989ab5f2272199e7cedbf503821d238
eLmincfrLu <elmincfrlu@gmail.com>
2026-02-01 10:48:52 +0000

    python

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - Goes through a binary tree using post-order traversal
+ * @tree: Pointer to the root node
+ * @func: Pointer to a function to call for each node
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+
+	/* Sol subtree-i post-order ilə ziyarət et */
+	binary_tree_postorder(tree->left, func);
+
+	/* Sağ subtree-i post-order ilə ziyarət et */
+	binary_tree_postorder(tree->right, func);
+
+	/* Node-u ziyarət et */
+	func(tree->n);
+}
